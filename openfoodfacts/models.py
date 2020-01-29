0d47b6ad5a596f72c7231e0be4d2c0bdd22b2136
ArthurNeyer <arthur.neyer@protonmail.com>
2020-01-29 15:23:48 +0100

    Add docstrings

--- openfoodfacts/models.py
+++ openfoodfacts/models.py
@@ -1,12 +1,13 @@
 import sys
 from pony.orm import *
 
-from . import settings
 from .settings import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB
 
 db = Database()
 
 class Food(db.Entity):
+    """ Food Class contains the data of openfoodfacts """
+
     id = PrimaryKey(int, auto=True)
     name = Required(str)
     categories = Set('Category')
@@ -16,23 +17,34 @@ class Food(db.Entity):
     favor = Optional('Favor')
 
 class Category(db.Entity):
+    """ Category Class is connected to food by a many-to-many relation. """
+
     id = PrimaryKey(int, auto=True)
     name = Required(str)
     foods = Set(Food)
 
 class Brand(db.Entity):
+    """ Brand Class is connected to food by a many-to-many relation. """
+
     id = PrimaryKey(int, auto=True)
     name = Required(str)
     foods = Set(Food)
 
 class Favor(db.Entity):
+    """ Favor Class is connected to food by a one-to-one relation. """
+
     id = PrimaryKey(int, auto=True)
     food = Required(Food)
 
+# Connect to database
 db.bind(provider='mysql', host=MYSQL_HOST, user=MYSQL_USER,
     passwd=MYSQL_PASSWORD, db=MYSQL_DB)
+
+# Generate database mapping and create tables if they doesn't exists
 db.generate_mapping(create_tables=True)
 
-if(settings.DEBUG):
-    db.drop_all_tables(with_all_data=True)
-    db.create_tables()
+# This lines delete and rebuild the tables if we fill the database
+if(len(sys.argv) > 0):
+    if('fill_database' in sys.argv):
+        db.drop_all_tables(with_all_data=True)
+        db.create_tables()
